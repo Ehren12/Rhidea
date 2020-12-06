56322003b9ab36c1f7c96b51154f8d6b0f402998
Ehren <nwokochaehren@gmail.com>
2020-12-06 10:51:26 +0100

    minor design fixes

---
+++ models/Topics.js
@@ -0,0 +1,52 @@
+
+const mongoose = require('mongoose')
+const User = require('./User')
+const Ideas = require('./User')
+
+const IdeaSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+
+    description: {
+        type: String,
+        required: true
+    },
+
+    problem: {
+        type: String,
+        required: false 
+    },
+
+    status: {
+        type: String,
+        default: 'public',
+        enum: ['public', 'private'],
+        required: true
+    },
+
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+
+    createdAt: {
+        type: Date,
+        created: Date
+    },
+
+    tags: {
+        type: String,
+        trim: true,
+        lowercase: true,
+    },
+
+    bulbs: {
+        type: Number,
+    },
+})
+
+module.exports = mongoose.model('Idea', IdeaSchema)
